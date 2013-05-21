1328a27d833483b660ab4c04b2e1eba5efc1a095
puterjam <puterjam@gmail.com>
2013-05-21 11:59:28 +0800

    html5的签名档工具启动

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-Signature-
+Signature!
 ==========
 
 HTML5版本的在线签名档工具。
+
+#### 版本以及作者信息
+**版本:** V0.42
+
+**作者:** FrontEnd Team from Tencent SNS department.
+****
+
+#### 使用说明
+本地直接访问`index.html`即可， 当然你也可以上传到服务器。
+
+我们不提供模板制作的支持。
+****
+
+#### 模板制作
+提供图片的DataURI或者具体的图片路径（图片路径需要避免跨域问题）。
+
+添加新模板在 `js/themes.js` 中编辑即可。模板范例如下:
+
+	/**
+	 * 主题范例
+	 * @type {Object}
+	 */
+	var THEME2 = {
+		bg: 'data:image/png;base64,iVBORw0KGgoAAA...',
+		width: 680,
+		height: 140,
+		name: "纯白",
+		textStyle: {
+			name: {
+				font: "bold 28px Ubuntu",
+				x: 169,
+				y: 48,
+				color: "#4C4C4C"
+			},
+			group: {
+				font: "16px Ubuntu",
+				x: "175 + %name%",
+				y: 48,
+				color: "#FF6A00"
+			},
+			phone: {
+				font: "12px Ubuntu",
+				x: 188,
+				y: 89,
+				color: "#666666"
+			},
+			mobile: {
+				font: "12px Ubuntu",
+				x: 358,
+				y: 89,
+				color: "#666666"
+			}
+		}
+
+****
+
+#### HTML5技术支持
+使用到的H5的技术细节
+>1. <a href="https://developer.mozilla.org/en-US/docs/HTML/Canvas" target="_blank">Canvas</a>
+2. <a href="https://developer.mozilla.org/zh-CN/docs/DOM/window.atob" target="_blank">window.atob</a>
+3. <a href="https://developer.mozilla.org/zh-CN/docs/DOM/Blob" target="_blank">Blob</a>
+4. <a href="https://developer.mozilla.org/zh-CN/docs/DOM/window.URL.createObjectURL" target="_blank">URL.createObjectURL</a>
+5. <a href="https://developer.mozilla.org/en-US/docs/JavaScript/Typed_arrays" target="_blank">JavaScript typed arrays</a>
+6. <a href="http://updates.html5rocks.com/2011/08/Downloading-resources-in-HTML5-a-download" target="_blank">a[download]</a>
+7. <a href="http://www.google.com/fonts/" target="_blank">Google Fonts</a>
+
+****
+
+#### LICENSE
+http://www.apache.org/licenses/LICENSE-2.0.txt
---
+++ js/signature.js
@@ -0,0 +1,205 @@
+/*!
+* signature.js by @puterjam
+* Copyright 2013 Tencent, Inc.
+* http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
+
+/**
+ * 签名档库
+ * @class Signature
+ */
+(function Signature() {
+	/**
+	 * 列表模板
+	 * @type {Array}
+	 */
+	var ITEM_HTML = [
+		'<div style="text-align: center">',
+		'	<p>',
+		'		<img id="%sign_img%" style="width:%width%;height:%height%;max-width:100%" class="img-polaroid"/>',
+		'		<canvas id="%sign_canvas%" width="%width%" height="%height%" style="display:none"></canvas>',
+		'	</p>',
+		'	<p>',
+		'		<a class="btn btn-small btn-success" canvas_id="%sign_canvas%" download="%id%.png" target="_blank">保存 [%name%]</a>',
+		'	</p>',
+		'</div>'].join("");
+
+	//TODO 暂时放这里先吧，其实不太好
+	//获取当前的主题配置
+	var currentThemes = window.THEME_CONFIG;
+
+	/**
+	 * 绘制主题列表页面
+	 */
+	var drawPage = function() {
+		var html = [];
+		for (var k in currentThemes) {
+			var _item = ITEM_HTML.replace(/%id%/g, k)
+				.replace(/%sign_img%/g, k + "_img")
+				.replace(/%sign_canvas%/g, k + "_canvas")
+				.replace(/%name%/g, currentThemes[k].name)
+				.replace(/%width%/g, currentThemes[k].width)
+				.replace(/%height%/g, currentThemes[k].height);
+
+			html.push(_item);
+		}
+
+		$("#theme_list").html(html);
+
+		//渲染所有页面上的签名档
+		renderAllSign();
+	}
+
+	var renderAllSign = function() {
+		for (var k in currentThemes) {
+			renderSign(k);
+		}
+	}
+
+	/**
+	 * 渲染签名档
+	 * @param {String} themeId 主题id
+	 */
+	var renderSign = function renderSign(themeId) {
+		// console.log("render:" + themeId);
+		var canvas = document.getElementById(themeId + "_canvas");
+		var context = canvas.getContext("2d");
+
+		var img = new Image();
+
+		//背景图片加载完成，开始填充canvas
+		img.onload = function() {
+			(_drawSign.bind(this))(context, currentThemes[themeId]);
+
+			//显示预览图
+			var _preview = document.getElementById(themeId + "_img");
+			_preview.src = canvas.toDataURL("image/png");
+			_preview.title = "size:" + canvas.width + "x" + canvas.height;
+		}
+
+		//初始化背景图片
+		img.src = currentThemes[themeId].bg;
+	}
+
+	/**
+	 * 绘制签名档
+	 * @param  {context} ctx   画布的内容区
+	 * @param  {Object} theme 主题配置
+	 */
+	var _drawSign = function drawSign(ctx, theme) {
+		theme._text = theme._text || {};
+
+		//绘制背景图片
+		ctx.drawImage(this, 0, 0, this.width, this.height);
+
+		//绘制名称
+		_fillText(ctx, $("#rtx_input").val(), theme, "name");
+
+		//绘制团队名称
+		//var w = ctx.measureText(name).width;
+		_fillText(ctx, "[" + $("#group_input").val() + "]", theme, "group");
+
+		//绘制电话号码
+		_fillText(ctx, "0755-86013388-" + $("#phone_input").val(), theme, "phone");
+
+		//绘制手机号码
+		_fillText(ctx, "86-" + $("#mobile_input").val(), theme, "mobile");
+	}
+
+	/**
+	 * 填充文字
+	 * @param  {context} ctx    canvas context 2d 内容
+	 * @param  {string} text   填充文字
+	 * @param  {Object} theme  主题
+	 * @param  {string} textId 文本id
+	 */
+	var _fillText = function fillText(ctx, text, theme, textId) {
+		var format = theme.textStyle[textId];
+		var x = _fixPosition(ctx, theme, format.x, "x"),
+			y = _fixPosition(ctx, theme, format.y, "y");
+
+		//setup font style
+		ctx.font = format.font;
+
+		//fill color
+		ctx.fillStyle = format.color;
+
+		//fill text
+		ctx.fillText(text, x, y);
+
+		theme._text[textId] = text;
+	}
+
+	var _fixPosition = function(ctx, theme, value, pos) {
+		if (typeof value == "number") {
+			return value;
+		} else {
+			var _v = value.replace(/\%([^%]+)\%/g, function() {
+				var textId = arguments[1];
+
+				//setup font style
+				ctx.font = theme.textStyle[textId].font;
+				var _m = ctx.measureText(theme._text[textId]);
+
+				return (pos == "x" ? _m.width : _m.height);
+			});
+			return eval(_v);
+		}
+	}
+
+	/**
+	 * DataURI转换成Blob格式
+	 * @param  {string} dataURI  base64 的 data uri
+	 * @param  {string} mimetype 需要输出的blob的mimetype格式
+	 * @return {Blob}   大二进制数据块
+	 */
+	var convertDataURIToBlob = function convertDataURIToBlob(dataURI, mimetype) {
+		//分割DataURI成 base64格式;
+		var BASE64_HEADER = ';base64,';
+		var base64Index = dataURI.indexOf(BASE64_HEADER) + BASE64_HEADER.length;
+		var base64 = dataURI.substring(base64Index);
+
+		//将已经被base64编码过的数据进行解码
+		var raw = window.atob(base64); // more https://developer.mozilla.org/zh-CN/docs/DOM/window.atob
+		var rawLength = raw.length;
+
+		//编码成 8 位无符号整数值的类型化数组
+		var uInt8Array = new Uint8Array(rawLength);
+		for (var i = 0; i < rawLength; ++i) {
+			uInt8Array[i] = raw.charCodeAt(i);
+		}
+
+		//返回blob对象
+		return new Blob([uInt8Array], {
+			type: mimetype
+		}); // more https://developer.mozilla.org/zh-CN/docs/DOM/Blob
+	}
+
+	//代码初始化入口
+	$(document).ready(function init() {
+		//绑定输入框的键盘事件
+		$("#theme_config input").on("keyup", renderAllSign);
+
+		//绑定主题列表的全局单击事件
+		$("#theme_list").on("click", function(e) {
+			var target = e.target;
+			var canvasId = target.getAttribute("canvas_id");
+
+			//判断对象是否是按钮
+			if (canvasId) {
+				var canvas = document.getElementById(canvasId);
+
+				//把base64转换成blob
+				var blob = convertDataURIToBlob(canvas.toDataURL("image/png"), "image/png");
+				target.href = URL.createObjectURL(blob); // more http://updates.html5rocks.com/2011/08/Downloading-resources-in-HTML5-a-download
+
+				//5秒后释放 URL 以节约内存
+				setTimeout(function() {
+					URL.revokeObjectURL(blob);
+				}, 5000);
+			}
+		});
+
+		drawPage();
+	});
+})();
